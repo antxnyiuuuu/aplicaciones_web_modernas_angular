a8555940f88c043296fedb44140b8b3e4165081a
antxnyiuuuu <antonychaguamate@gmail.com>
2025-11-24 17:57:04 -0500

    Reto 6

--- pratica-angular/aprender-angular/src/app/perfil/perfil.ts
+++ pratica-angular/aprender-angular/src/app/perfil/perfil.ts
@@ -9,6 +9,13 @@ import { Component } from "@angular/core";
 )
 
 export class PerfilComponent {
+
+    public nombre: string = "Antony";
+    public edad: number = 30;
+    public direccion: string = "Av. Amazonas y Río Coca, Quito, Ecuador";
+    public telefono: string = "+593 999 999 999";
+    public correo: string = "Antony@gmail.com";
+
     constructor() {
         alert("¡Bienvenido al perfil, estás dentro de la aplicación!");
         console.log("¡Bienvenido al perfil, estás dentro de la aplicación!");
