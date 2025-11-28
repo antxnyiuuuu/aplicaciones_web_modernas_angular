f280f6580cd1b8d6b67a8e7efa4f6a8459440788
antxnyiuuuu <antonychaguamate@gmail.com>
2025-11-27 23:30:36 -0500

    reto 9

--- pratica-angular/aprender-angular/src/app/perfil/perfil.ts
+++ pratica-angular/aprender-angular/src/app/perfil/perfil.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, DoCheck } from "@angular/core";
 import { Cuenta } from "../cuenta/cuenta";
 import { Transacciones } from "../transacciones/transacciones";
 import { Prestamos } from "../prestamos/prestamos";
@@ -12,7 +12,7 @@ import { Prestamos } from "../prestamos/prestamos";
 
 )
 
-export class PerfilComponent {
+export class PerfilComponent implements DoCheck {
 
     public nombre: string = "Antony";
     public edad: number = 30;
@@ -20,9 +20,28 @@ export class PerfilComponent {
     public telefono: string = "+593 999 999 999";
     public correo: string = "Antony@gmail.com";
 
+    private edadAnterior: number = 30;
+    private direccionAnterior: string = "Av. Amazonas y Río Coca, Quito, Ecuador";
+
     constructor() {
         alert("¡Bienvenido al perfil, estás dentro de la aplicación!");
         console.log("¡Bienvenido al perfil, estás dentro de la aplicación!");
+    }
+
+    ngDoCheck(): void {
+        if (this.edad !== this.edadAnterior || this.direccion !== this.direccionAnterior) {
+            console.log("Componente actualizado");
+            this.edadAnterior = this.edad;
+            this.direccionAnterior = this.direccion;
+        }
+    }
+
+    cambiarEdad(): void {
+        this.edad = Math.floor(Math.random() * 50) + 20;
+    }
+
+    cambiarDireccion(): void {
+        this.direccion = "Nueva dirección " + Math.floor(Math.random() * 1000);
     }
 
 }
