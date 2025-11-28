373a1b6a6ec2b6bd4d8bf465eb2c7affb2d3efdc
antxnyiuuuu <antonychaguamate@gmail.com>
2025-11-28 15:17:53 -0500

    Reto 14

--- pratica-angular/aprender-angular/src/app/app.ts
+++ pratica-angular/aprender-angular/src/app/app.ts
@@ -6,7 +6,7 @@ import { Prestamos } from './prestamos/prestamos';
 
 @Component({
   selector: 'app-root',
-  imports: [ MiPerfil],
+  imports: [MiPerfil],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
