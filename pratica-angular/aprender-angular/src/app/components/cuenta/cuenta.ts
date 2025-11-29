62e9fd982c2856ddfbf3d38800be8a80ea9b2018
antxnyiuuuu <antonychaguamate@gmail.com>
2025-11-28 22:19:20 -0500

    Reto 21

--- pratica-angular/aprender-angular/src/app/components/cuenta/cuenta.ts
+++ pratica-angular/aprender-angular/src/app/components/cuenta/cuenta.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Cliente } from '../../models/cliente';
 @Component({
   selector: 'app-cuenta',
+  standalone: true,
   imports: [],
   templateUrl: './cuenta.html',
-  styleUrl: './cuenta.css',
+  styleUrls: ['./cuenta.css'],
 })
 export class Cuenta {
   public mostrarCuenta: boolean = true;
   public cliente: Cliente;
   constructor() {
-    this.cliente = new Cliente(1, 'Antony', 'Chaguamate', 'antonykhujksad@gmail.com', 5000);
+    this.cliente = new Cliente(1, 'Antony', 'Chaguamate', 'antony@gmail.com', 5000);
   }
+  @Input() nombreCliente: string = '';
   mostrarCompCuenta(valor: boolean) {
     this.mostrarCuenta = valor;
   }
